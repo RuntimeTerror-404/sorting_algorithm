a7500cec400a17cf6e7a99ae148335e39b9d01a7
Mohit Parashar <mohit1672002@gmail.com>
2021-02-12 10:20:28 +0530

    first commit

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,6 +2,23 @@
 
 using namespace std;
 
+void insertionSort(int a[], int n){
+    for(int i=1; i<n; i++){
+        int current = a[i];
+        int j = i-1;
+        while(a[j]>current && j>=0){
+            a[j+1] = a[j];
+            j--;
+        }
+        a[j+1] = current;
+    }
+}
+
 int main(){
-    cout<<"Hello World from Mohit Programmer";
+    int a[] = {21, 10, 17, 32, 90, 41};
+    int n = sizeof(a)/sizeof(int);
+    insertionSort(a,n);
+    for(int i=0; i<n; i++){
+        cout<<a[i]<<" ";
+    }
 }
